742464f4def7d62a78bb64d76cc2b4815dfaa467
Clemente Nogueira <git.3gear@gmail.com>
2023-06-26 16:48:14 +0100

    The Demo component logic has been created and configured.

--- project/src/services/article.js
+++ project/src/services/article.js
@@ -1,10 +1,22 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const api_key = import.meta.env.VITE_MY_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
     reducerPath: "articleApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+        prepareHeaders: (headers) => {
+            headers.set("X-RapidAPI-Key", api_key);
+            headers.set("X-RapidAPI-Host", "article-extractor-and-summarizer.p.rapidapi.com");
+            return headers;
+        },
+    }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => `article/summary/${params.id}`,
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
         }),
     }),
 });
+
+export const {useLazyGetSummaryQuery} = articleApi;
